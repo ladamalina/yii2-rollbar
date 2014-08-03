0e5286b8931952333ce6533ca96f6d5bd3c6e773
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2014-08-03 18:50:07 +0400

    component

---
+++ src/RollbarComponent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ladamalina\yii2-rollbar;
+
+class RollbarComponent extends \yii\base\Object
+{
+    // required
+    public $accessToken;
+
+    // optional
+    public $environment;
+    public $branch;
+    public $batched;
+    public $batchSize;
+    public $timeout;
+    public $logger;
+    public $maxErrno;
+    public $baseApiUrl;
+    public $rootAlias = '@app';
+
+    public function init()
+    {
+        \Rollbar::init(
+            [
+                'access_token' => $this->accessToken,
+                'environment' => $this->environment,
+                'branch' => $this->branch,
+                'batched' => $this->batched,
+                'batch_size' => $this->batchSize,
+                'timeout' => $this->timeout,
+                'logger' => $this->logger,
+                'max_errno' => $this->maxErrno,
+                'base_api_url' => $this->baseApiUrl,
+                'root' => !empty($this->rootAlias) ? Yii::getAlias($this->rootAlias) : '',
+            ],
+            false,
+            false
+        );
+
+        parent::init();
+    }
+}
